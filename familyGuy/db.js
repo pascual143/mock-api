a68bb7d8eabc734255a5c3e22b3893493253d617
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2025-01-08 13:46:45 +0100

    Create db.js

---
+++ familyGuy/db.js
@@ -0,0 +1,20 @@
+{
+  "characters": [
+    { "id": 1, "name": "Peter Griffin", "role": "Father", "description": "The bumbling, often clueless father of the Griffin family." },
+    { "id": 2, "name": "Lois Griffin", "role": "Mother", "description": "The intelligent, often patient wife of Peter." },
+    { "id": 3, "name": "Stewie Griffin", "role": "Son", "description": "The evil genius baby with a British accent." },
+    { "id": 4, "name": "Brian Griffin", "role": "Dog", "description": "A talking dog who is also Peter's best friend." },
+    { "id": 5, "name": "Meg Griffin", "role": "Daughter", "description": "The awkward, often ignored teenage daughter." }
+  ],
+  "episodes": [
+    { "id": 1, "title": "Road to the Multiverse", "season": 8, "episode": 1, "description": "Brian and Stewie travel to different alternate universes." },
+    { "id": 2, "title": "Petarded", "season": 4, "episode": 6, "description": "Peter gets tested for being mentally retarded and believes he is the smartest man in the world." },
+    { "id": 3, "title": "Blue Harvest", "season": 6, "episode": 1, "description": "The Griffins retell the story of 'Star Wars: A New Hope' with themselves as the characters." }
+  ],
+  "quotes": [
+    { "id": 1, "character": "Peter Griffin", "quote": "Freakin' sweet!" },
+    { "id": 2, "character": "Stewie Griffin", "quote": "Victory is mine!" },
+    { "id": 3, "character": "Brian Griffin", "quote": "I’m just a dog, I can’t do this." },
+    { "id": 4, "character": "Lois Griffin", "quote": "Peter, you can’t just do whatever you want!" }
+  ]
+}
